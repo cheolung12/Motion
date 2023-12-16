44747f720f471e0332270ab9995180517c1d948c
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-12-17 02:56:49 +0900

    Image Component

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
-import { PageComponent } from './components/page.js';
+import { ImageComponent } from './components/pages/item/image.js';
+import { PageComponent } from './components/pages/page.js';
 
 class App {
     private readonly page: PageComponent;
     constructor(appRoot: HTMLElement) {
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
+
+        const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
+        image.attachTo(appRoot, 'beforeend');
     }
 }
 
