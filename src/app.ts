b04e7ad8688b92760fc88144d42d7d4ceeb0593a
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-12-24 22:38:51 +0900

    add components

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import { ImageComponent } from './components/pages/item/image.js';
+import { NoteComponent } from './components/pages/item/note.js';
+import { VideoComponent } from './components/pages/item/video.js';
 import { PageComponent } from './components/pages/page.js';
 
 class App {
@@ -9,6 +11,12 @@ class App {
 
         const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
         image.attachTo(appRoot, 'beforeend');
+
+        const note = new NoteComponent('Note Title', 'Note Body');
+        note.attachTo(appRoot, 'beforeend');
+
+        const video = new VideoComponent('Video Title', 'https://youtu.be/UMYlSnLlCE0');
+        video.attachTo(appRoot, 'beforeend');
     }
 }
 
