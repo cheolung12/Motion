2222ea500f821c52018719dae3d863145ff7797e
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-12-26 23:54:04 +0900

    page item component

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,23 @@
+import { Component } from './components/component.js';
 import { ImageComponent } from './components/pages/item/image.js';
 import { NoteComponent } from './components/pages/item/note.js';
 import { VideoComponent } from './components/pages/item/video.js';
-import { PageComponent } from './components/pages/page.js';
+import { Composable, PageComponent } from './components/pages/page.js';
 
 class App {
-    private readonly page: PageComponent;
+    private readonly page: Component & Composable;
     constructor(appRoot: HTMLElement) {
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
 
         const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
-        image.attachTo(appRoot, 'beforeend');
+        this.page.addChild(image);
 
         const note = new NoteComponent('Note Title', 'Note Body');
-        note.attachTo(appRoot, 'beforeend');
+        this.page.addChild(note);
 
         const video = new VideoComponent('Video Title', 'https://youtu.be/UMYlSnLlCE0');
-        video.attachTo(appRoot, 'beforeend');
+        this.page.addChild(video);
     }
 }
 
